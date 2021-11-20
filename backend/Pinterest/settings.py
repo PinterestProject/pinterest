b5bff21ff0a28b11d87329e04065498017cfc7d1
KhalilGazairly <khalil.gazairly@gmail.com>
2021-11-20 16:54:32 +0200

    firstcommit

--- backend/Pinterest/settings.py
+++ backend/Pinterest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'Users',
     'Boards',
     'Pins',
@@ -84,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pinterest',
         'USER': 'postgres',
-        'PASSWORD':'691999',
+        'PASSWORD':'goegoegoe123',
         'HOST':'localhost',
         'PORT': '5432'
 
