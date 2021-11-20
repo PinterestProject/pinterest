a1470f727213e5f944c3b6db9593a6bb1169f9d4
Mark Gamal <markgamalm@gmail.com>
2021-11-20 22:47:25 +0200

    1. Create Pin and Board models

--- backend/Pins/models.py
+++ backend/Pins/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from Users.models import User
+from Boards.models import Board
+from Categories.models import Category
 
-# Create your models here.
+
+class Pin(models.Model):
+    """
+    Pins Table
+    """
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    attachment = models.ImageField(upload_to="/uploads/pins/")
+    users = models.ManyToManyField(User)
+    boards = models.ManyToManyField(Board)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
