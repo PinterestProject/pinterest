c6837540c32ea75c16db40be8abba45a0b2b0e9e
KhalilGazairly <khalil.gazairly@gmail.com>
2021-11-21 00:44:15 +0200

    admin register

--- backend/Users/admin.py
+++ backend/Users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Invitation, User_board
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Invitation)
+admin.site.register(User_board)
--- backend/Users/models.py
+++ backend/Users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, UserManager
 from django.contrib.auth import get_user_model
-from Boards.models import Board
+# from Boards.models import Board
 
 class UserManager(BaseUserManager):
     """
