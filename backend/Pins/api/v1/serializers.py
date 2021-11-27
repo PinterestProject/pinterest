78553d009d831981a98be991d08b8b900967ce51
Mark Gamal <markgamalm@gmail.com>
2021-11-27 22:06:10 +0200

    Get username and profile_image along with other pin data

--- backend/Pins/api/v1/serializers.py
+++ backend/Pins/api/v1/serializers.py
@@ -1,8 +1,15 @@
 from Pins.models import Pin
 from rest_framework import serializers
+from Users.serializers import UserSerializer
 
 
 class PinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pin
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["username"] = UserSerializer(instance.user_id).data["username"]
+        representation["profile_image"] = UserSerializer(instance.user_id).data["profile_image"]
+        return representation
