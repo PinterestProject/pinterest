e4037f5d53d6a1d175a2ede5a1abbe4851d0a268
nesrensamir <86182948+nesrensamir@users.noreply.github.com>
2021-12-04 08:35:34 +0200

    add URL of "pins filter fn "

--- backend/Pins/api/v1/urls.py
+++ backend/Pins/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework import views
 
-from Pins.api.v1.views import PinDetails, PinList, get_board_pins, get_user_pins
+from Pins.api.v1.views import PinDetails, PinList, get_board_pins, get_user_pins, pinsOfSpecificCategory,
 
 app_name = "Pins-v1"
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("api/v1/pins/<int:pk>/", PinDetails.as_view(), name="pin-details"),
     path("api/v1/boards/<int:pk>/pins/", get_board_pins, name="board-pin-details"),
     path("api/v1/user/pins/", get_user_pins, name="get-user-pins"),
+    path("api/v1/categories/pins/",pinsOfSpecificCategory),
 ]
