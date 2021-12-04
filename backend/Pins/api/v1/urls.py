5ae1eda4db612cbd9f32a7425fc3b69a87fb2aa2
Mark Gamal <markgamalm@gmail.com>
2021-12-05 01:02:25 +0200

    fix pins urls

--- backend/Pins/api/v1/urls.py
+++ backend/Pins/api/v1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework import views
 
-from Pins.api.v1.views import PinDetails, PinList, get_board_pins, get_user_pins, pinsOfSpecificCategory,
+from Pins.api.v1.views import (
+    PinDetails,
+    PinList,
+    get_board_pins,
+    get_user_pins,
+    pinsOfSpecificCategory,
+)
 
 app_name = "Pins-v1"
 
@@ -10,5 +16,5 @@ urlpatterns = [
     path("api/v1/pins/<int:pk>/", PinDetails.as_view(), name="pin-details"),
     path("api/v1/boards/<int:pk>/pins/", get_board_pins, name="board-pin-details"),
     path("api/v1/user/pins/", get_user_pins, name="get-user-pins"),
-    path("api/v1/categories/pins/",pinsOfSpecificCategory),
+    path("api/v1/categories/pins/", pinsOfSpecificCategory),
 ]
